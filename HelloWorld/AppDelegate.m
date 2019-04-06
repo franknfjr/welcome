094138a8dde6afb2627b91ea435ef7026e81726e
franknfjr <frankdonascimentojr@gmail.com>
2019-04-06 15:20:58 -0300

    importando o view controller no app delegate com um label text

--- HelloWorld/AppDelegate.m
+++ HelloWorld/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FirstViewController.h"
 
 @interface AppDelegate ()
 
@@ -19,6 +20,11 @@
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    //cria o view controller principal
+    FirstViewController *vc = [[FirstViewController  alloc] init];
+    self.window.rootViewController = vc;
+    
     [self.window makeKeyAndVisible];
 
     return YES;
